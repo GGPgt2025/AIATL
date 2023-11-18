2e858afe45b01988093ab002a79822d21b92d535
awang418 <awang418@gatech.edu>
2023-11-18 18:03:15 -0500

    Langchain

--- analysis/analyze_10k.py
+++ analysis/analyze_10k.py
@@ -6,15 +6,28 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 
+# Function to fetch the 10-K filing data
+def fetch_10k_data(symbol):
+    # Fetch the latest 10-K filing URL using the Edgar API
+    edgar_api_url = f"https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={symbol}&type=10-K&dateb=&owner=exclude&count=1"
+    edgar_api_response = requests.get(edgar_api_url)
+    edgar_api_data = edgar_api_response.json()
+
+    # Check if the response contains filing details
+    if 'filings' in edgar_api_data and 'filings' in edgar_api_data['filings']:
+        latest_filing = edgar_api_data['filings']['filings'][0]
+        filing_url = latest_filing['href']
+
+        # Fetch the content of the latest 10-K filing
+        response = requests.get(filing_url)
+        return response.text
+    else:
+        return None
+
 # Function to analyze 10-K
-def analyze_10k(symbol):
+def analyze_10k(html_content):
 
-    # Fetch the content of the URL
-    url = "https://www.sec.gov/ix?doc=/Archives/edgar/data/320193/000032019323000106/aapl-20230930.htm"
-    response = requests.get(url)
-    html_content = response.text
-
-    # Load content from the URL using HTMLTextLoader
+    # Load content from the URL using UnstructuredHTMLTextLoader
     loader = UnstructuredHTMLLoader(html_content)
     documents = loader.load()
 
--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from analysis.analyze_10k import analyze_10k
+from analysis.analyze_10k import analyze_10k, fetch_10k_data
 from analysis.analyze_yahoo import fetch_stock_data
 
 st.title("Stock Pitch Deck Generator")
@@ -7,16 +7,22 @@ st.title("Stock Pitch Deck Generator")
 # User input
 symbol = st.text_input("Enter Stock Symbol (e.g., AAPL):").upper()
 
-# Fetch real-time stock data
-stock_data = analyze_10k(symbol)
-
-if stock_data:
-    st.subheader("Real-Time Stock Data:")
-    st.write(f"Symbol: {symbol}")
-    st.write(f"Current Price: {stock_data['c']}")
-
-    # # Fetch 10-K data (replace this with actual data fetching logic)
-    # ten_k_text = fetch_10k_data(symbol)
+if st.button("Generate Analysis"):
+    if symbol:
+        # st.subheader("Real-Time Stock Data:")
+        st.write(f"Symbol: {symbol}")
+        # st.write(f"Current Price: {stock_data['c']}")
+        # Fetch 10-K data
+        html_content = fetch_10k_data(symbol)
+        
+        if html_content:
+            # Analyze 10-K data
+            result = analyze_10k(html_content)
+            st.write(result)
+        else:
+            st.warning(f"No 10-K filings found for the symbol {symbol}.")
+    else:
+        st.warning("Please enter a stock symbol.")
 
     # # Analyze 10-K data
     # if ten_k_text:
