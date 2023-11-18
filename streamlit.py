78dd9c733b4c2dbc8f9cb222d78dbc9be5c37350
William Swann <135576368+rocketboy703@users.noreply.github.com>
2023-11-18 10:58:12 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,5 +3,8 @@ import streamlit as st
 # Streamlit app
 st.title("Stock Pitch Deck Generator")
 
+#Description
+st.write("This program generates a basic level stock picth given a specific stock sybol..")
+
 # User input
 symbol = st.text_input("Enter Stock Symbol from S&P500 (e.g., AAPL):").upper()
