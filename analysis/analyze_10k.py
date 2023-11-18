0117ea3a38ab7a5208bccb60d581e6368ded343a
GGPGTpro23 <gpalomino3@gatech.edu>
2023-11-18 12:38:17 -0500

    analysis

--- analysis/analyze_10k.py
+++ analysis/analyze_10k.py
@@ -1,5 +1,5 @@
 import requests
-from langchain.document_loaders import UnsctructuredHTMLLoader
+from langchain.document_loaders import UnstructuredHTMLLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
