c9f9a0446f599ee1997470425d6e197ae3a9057a
GGPGTpro23 <gpalomino3@gatech.edu>
2023-11-18 12:41:54 -0500

    analysis

--- streamlit.py
+++ streamlit.py
@@ -8,21 +8,21 @@ st.title("Stock Pitch Deck Generator")
 symbol = st.text_input("Enter Stock Symbol (e.g., AAPL):").upper()
 
 # Fetch real-time stock data
-stock_data = fetch_stock_data(symbol)
+stock_data = analyze_10k(symbol)
 
-# if stock_data:
-#     st.subheader("Real-Time Stock Data:")
-#     st.write(f"Symbol: {symbol}")
-#     st.write(f"Current Price: {stock_data['c']}")
+if stock_data:
+    st.subheader("Real-Time Stock Data:")
+    st.write(f"Symbol: {symbol}")
+    st.write(f"Current Price: {stock_data['c']}")
 
-#     # Fetch 10-K data (replace this with actual data fetching logic)
-#     ten_k_text = fetch_10k_data(symbol)
+    # # Fetch 10-K data (replace this with actual data fetching logic)
+    # ten_k_text = fetch_10k_data(symbol)
 
-#     # Analyze 10-K data
-#     if ten_k_text:
-#         analysis_result = analyze_10k(ten_k_text)
-#         display_results(analysis_result)
-#     else:
-#         st.warning("Failed to fetch 10-K data. Please check the symbol.")
-# else:
-#     st.warning("Failed to fetch real-time stock data. Please check the symbol or try again later.")
+    # # Analyze 10-K data
+    # if ten_k_text:
+    #     analysis_result = analyze_10k(ten_k_text)
+    #     display_results(analysis_result)
+    else:
+        st.warning("Failed to fetch 10-K data. Please check the symbol.")
+else:
+    st.warning("Failed to fetch real-time stock data. Please check the symbol or try again later.")
