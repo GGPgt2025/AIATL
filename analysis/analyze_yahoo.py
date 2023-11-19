2314a6ed3955fd2f9d73de29e7fe00dc20bf8dbc
William Swann <135576368+rocketboy703@users.noreply.github.com>
2023-11-19 14:46:41 -0500

    Update analyze_yahoo.py

--- analysis/analyze_yahoo.py
+++ analysis/analyze_yahoo.py
@@ -4,14 +4,36 @@ import streamlit as st
 import plotly.express as px
 from datetime import date, timedelta
 
+tabs = ["Prompt","Recommendation", "Company Background", "Investment Thesis", "Catalysts", "Comparable Analysis", "Risk Factors and Mitigating Risk Factors"]
+selected_tab = st.sidebar.radio("Select Tab", tabs)
+
+
 st.title("Stock Pitch Generator")
 symbol = st.text_input("Enter a stock symbol: ")
 ticker = yf.Ticker(symbol)
 
-start_date = date.today() - timedelta(days=3650)
-end_date = date.today()
+if selected_tab == "Recommendation":
+    st.header("Recommendation")
 
-data = yf.download(symbol, start=start_date, end=end_date)
-fig = px.line(data, x=data.index, y=data['Adj Close'], title=symbol)
-st.plotly_chart(fig)
+if selected_tab == "Company Background":
+    st.header("Company Background")
+    st.subheader("Past decade chart history:")
+    start_date = date.today() - timedelta(days=3650)
+    end_date = date.today()
+
+    data = yf.download(symbol, start=start_date, end=end_date)
+    fig = px.line(data, x=data.index, y=data['Adj Close'], title=symbol)
+    st.plotly_chart(fig)
+
+if selected_tab == "Investment Thesis":
+    st.header("Investment Thesis")
+
+if selected_tab == "Catalysts":
+    st.header("Catalysts")
+
+if selected_tab == "Comparable Analysis":
+    st.header("Comparable Analysis")
+
+if selected_tab == "Risk Factors and Mitigating Risk Factors":
+    st.header("Risk Factors and Mitigating Risk Factors")
 
