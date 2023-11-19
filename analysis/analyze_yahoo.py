130ed7b3c149c3d39ae368d3ca7b7a700d788b70
William Swann <135576368+rocketboy703@users.noreply.github.com>
2023-11-19 14:22:28 -0500

    Update analyze_yahoo.py

--- analysis/analyze_yahoo.py
+++ analysis/analyze_yahoo.py
@@ -1,17 +1,17 @@
 import yfinance as yf
+import requests
+import streamlit as st
+import plotly.express as px
+from datetime import date, timedelta
 
-# Function to fetch stock data from Yahoo Finance API
-def fetch_stock_data(symbol):
-    # Replace 'YOUR_YAHOO_API_KEY' with your actual Yahoo Finance API key
-    yahoo_api_key = 'YOUR_YAHOO_API_KEY'
-    
-    # Construct the API endpoint for real-time stock quotes
-    api_url = f'https://finnhub.io/api/v1/quote?symbol={symbol}&token={yahoo_api_key}'
+st.title("Sock Pitch Generator")
+symbol = st.text_input("Enter a stock symbol: ")
+ticker = yf.Ticker(symbol)
 
-    try:
-        response = requests.get(api_url)
-        data = response.json()
-        return data
-    except Exception as e:
-        st.error(f"Error fetching stock data: {e}")
-        return None
+start_date = date.today() - timedelta(days=3650)
+end_date = date.today()
+
+data = yf.download(symbol, start=start_date, end=end_date)
+fig = px.line(data, x=data.index, y=data['Adj Close'], title=symbol)
+st.plotly_chart(fig)
+
